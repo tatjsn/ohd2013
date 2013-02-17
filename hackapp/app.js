f0454b545119330f3259cadfa8ca4eff2a5f50c2
Yohei Abe <iehoy.eba@gmail.com>
2013-02-17 15:17:09 +0900

    htdocsを統合

--- hackapp/app.js
+++ hackapp/app.js
@@ -25,6 +25,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '../htdocs')));
 });
 
 app.configure('development', function(){
